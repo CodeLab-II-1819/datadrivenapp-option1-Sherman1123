fdd5f0df17fc021756fa8cee5964972d63113abe
andrew parsons <346706@bathspa.ac.uk>
2019-01-29 22:43:05 +0000

    Update ofApp.cpp

--- datadrivenapp-option1-Sherman1123/src/ofApp.cpp
+++ datadrivenapp-option1-Sherman1123/src/ofApp.cpp
@@ -2,8 +2,12 @@
 #include <fstream>
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+	btnImg.load("button.png");//load in button image
 
+	//set position and size for 2 button rectangles
+	btn1.set(20, 100, 200, 100);
+	btn2.set(250, 100, 50, 50);
 }
 
 //--------------------------------------------------------------
@@ -12,8 +16,17 @@ void ofApp::update(){
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
+	//set color to white - ensures no tint placed on image
+	ofSetColor(255);
+	//draw img and pass in btn1 rectangle object to set positions
+	btnImg.draw(btn1);
 
+	//set color to green
+	ofSetColor(0, 255, 0);
+	//draw button using drawRectangle and pass in rectangle positions from btn2
+	ofDrawRectangle(btn2);
+}
 }
 
 //--------------------------------------------------------------
@@ -37,8 +50,14 @@ void ofApp::mouseDragged(int x, int y, int button){
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
+void ofApp::mousePressed(int x, int y, int button) {
+	//check whether mouse press is inside buttons
+	if (btn1.inside(x, y)) {
+		std::cout << "Clicked button 1" << endl;
+	}
+	if (btn2.inside(x, y)) {
+		std::cout << "Clicked button 2" << endl;
+	}
 }
 
 //--------------------------------------------------------------
